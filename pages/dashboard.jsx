5041c9f3d0c7e30af51c9d0c2e77a0b976cee171
voorinc <voorinc@Mac-mini-de-voorinc.local>
2023-04-12 05:56:06 -0400

    add toke

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -2,14 +2,33 @@ import { signOut } from "firebase/auth";
 import { auth } from "../lib/firebase";
 
 export default function Dashboard() {
+  async function getToken() {
+    const user = await auth.currentUser;
+    console.log(user);
+    const token = await user.getIdToken(/* forceRefresh */ true);
+    console.log(token);
+
+  }
+
   return (
-    <div>
-      <button
-        className="bg-indigo-200 border p-12 hover:bg-indigo-400 "
-        onClick={() => signOut(auth)}
-      >
-        LOG OUT
-      </button>
-    </div>
+    <>
+      <div>
+        <button
+          className="bg-indigo-200 border p-12 hover:bg-indigo-400 "
+          onClick={() => signOut(auth)}
+        >
+          LOG OUT
+        </button>
+      </div>
+
+      <div>
+        <button
+          className="bg-indigo-200 border p-12 hover:bg-indigo-400 "
+          onClick={() => getToken()}
+        >
+          Token
+        </button>
+      </div>
+    </>
   );
 }
